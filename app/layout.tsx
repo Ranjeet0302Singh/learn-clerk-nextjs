64fba4d49cd3adfc9bf495fc79bc227843d0fb50
Ranjeet0302Singh <ranjeet843507@gmail.com>
2024-03-07 01:31:52 +0530

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import { ClerkProvider } from "@clerk/nextjs";
+import Header from "@/components/header";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,7 +17,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+        <body className={inter.className}>
+        <ClerkProvider>
+          <Header />
+          {children}
+    </ClerkProvider>
+        </body>
+      </html>
   );
 }
